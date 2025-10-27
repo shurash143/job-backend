59bcd8a8d020f4ef8e1b0606676399da10a99594
shurash143 <shukrigarad32@gmail.com>
2025-10-27 12:38:25 +0300

    g/

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use(router);
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`JSON Server running on port ${PORT}`);
 });
